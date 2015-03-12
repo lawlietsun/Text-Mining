85a711fc1a51d31f2b9e847b43446c335358b23d
lawlietsun <lawliet.sun@gmail.com>
2015-03-12 16:43:35 +0000

    DM course work

---
+++ exercise8.R
@@ -0,0 +1,7 @@
+setwd("~/Google Drive/MSc Data Analytics/Core/CS909 Data Mining/exercises/exercise8")
+
+mydata <- read.delim2("reuters.tsv", quote = "")
+
+str(mydata)
+
+readLines("reuters.tsv")[10]
